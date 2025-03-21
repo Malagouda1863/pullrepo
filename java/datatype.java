cbec3829955300f01eb7068536553846ac58f3a5
Malagouda1863 <malangoudapatil7788@gmail.com>
2025-03-21 10:13:12 +0530

    save java

---
+++ java/datatype.java
@@ -0,0 +1,31 @@
+package java;
+
+public class datatype {
+
+        public static void main(String[] args)
+        {
+            char c='c';
+            int a= 10;
+            byte b= 120;
+            short s=345;
+            float f=2.2f;
+            double d=34.56;
+            long l=45;
+    
+            System.out.println("char: "+c);
+            System.out.println("integer: "+a);
+    
+            System.out.println("byte: "+b);
+    
+            System.out.println("short: "+s);
+    
+            System.out.println("float: "+f);
+    
+            System.out.println("double: "+d);
+    
+            System.out.println("long : "+l);
+    
+        }
+    
+    
+}
